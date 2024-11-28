70e0516275943b074991c91c288205016c982c62
Taal-Turtat <82641334+Taal-Turtat@users.noreply.github.com>
2024-11-28 23:23:18 +0900

    そこそこ改変

--- JS/calendar.js
+++ JS/calendar.js
@@ -7,7 +7,7 @@ const calendarMake = () => {
     //頭の処理
     strShow += "<table align='right'><thead><tr>";
     strList.forEach((date) => {
-        let str = "<th class='"+ date +"'>" + date + "</th>";
+        let str = "<th class='heads' class='"+ date +"'>" + date + "</th>";
         strShow += str;
     });
     strShow += "</tr></thead>"
@@ -18,10 +18,10 @@ const calendarMake = () => {
     const dateStarts = 6; // 0:Mon ... 6:Sun
     let i = 1;
     strShow += "<tbody>";
-    while(i <= dayEnds+14){
+    while(i <= 42){
         strShow += "<tr>";
         for(let j=0;j<7;j++){
-            let str = "<td class='"+ strList[j]+"'";
+            let str = "<td class='days' class='"+ strList[j]+"'";
             if(i > dayShowingEnd+dateStarts){str += "class='pseudo'"}
             if(i > dateStarts && i <= (dayEnds+dateStarts)){ str += ">"+ (i-dateStarts) +"</td>"; }
             else{str += ">" + " " + "</td>";}
