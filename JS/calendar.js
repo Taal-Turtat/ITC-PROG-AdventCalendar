b4be57d879a4e6a848c4755b1c87931ab64735dc
Taal-Turtat <82641334+Taal-Turtat@users.noreply.github.com>
2024-11-28 21:19:43 +0900

    修正

--- JS/calendar.js
+++ JS/calendar.js
@@ -3,33 +3,35 @@ const calendar = document.getElementById("calendar");
 const calendarMake = () => {
     /* カレンダーを表示する関数 */
     let strList = ["Mon","Tue","Wed","Thr","Fri","Sat","Sun"];
+    let strShow = "";
     //頭の処理
-    calendar.innerHTML += "<table><thead><tr>";
-    strList.forEach(date => {
-        let str = "<th class='"+ date +"'>";
-        str += date + "</th>";
-        calendar.innerHTML += str;
+    strShow += "<table align='right'><thead><tr>";
+    strList.forEach((date) => {
+        let str = "<th class='"+ date +"'>" + date + "</th>";
+        strShow += str;
     });
-    calendar.innerHTML += "</tr></thead>"
+    strShow += "</tr></thead>"
     //カレンダー部分の処理
     const dayStarts = 1;
     const dayEnds = 31;
     const dayShowingEnd = 25;
     const dateStarts = 6; // 0:Mon ... 6:Sun
     let i = 0;
-    calendar.innerHTML += "<tbody>";
+    strShow += "<tbody>";
     while(i <= dayEnds+14){
-        calendar.innerHTML += "<tr>";
+        strShow += "<tr>";
         for(let j=0;j<7;j++){
             let str = "<td class='"+ strList[j]+"'";
             if(i > dayShowingEnd+dateStarts){str += "class='pseudo'"}
-            str += ">"+ i > dateStarts && i <= (dayEnds+dateStarts) ? (i-dateStarts).toString() : " " +"</td>";
-            calendar.innerHTML += str;
+            if(i > dateStarts && i <= (dayEnds+dateStarts)){ str += ">"+ (i-dateStarts) +"</td>"; }
+            else{str += ">" + " " + "</td>";}
+            strShow += str;
             i++;
         }
-        calendar.innerHTML += "</tr>";
+        strShow += "</tr>";
     }
-    calendar.innerHTML += "</tbody></table>";
+    strShow += "</tbody></table>";
+    calendar.innerHTML = strShow;
 }
 
 calendarMake();
